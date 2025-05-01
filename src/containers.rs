6c6641d5f5fe33900afbc5d8e2633a9d1725f633
Stephen Hara <stephen.hara@stephenhara.com>
2025-05-02 08:28:02 +0900

    better port map parsing

--- src/containers.rs
+++ src/containers.rs
@@ -1,10 +1,10 @@
 use serde::Serialize;
-use std::process::Command;
+use std::{fmt::Display, process::Command};
 
 #[derive(Serialize)]
 pub struct Container {
     id: String,
-    ports: String,
+    ports: std::vec::Vec<PortMapping>,
     name: String,
     image: String,
 }
@@ -13,9 +13,119 @@ impl Container {
     pub fn to_html_tr(&self) -> String {
         format!(
             "<tr><td>{}</td><td>{}</td><td>{}</td><td>{}</td></tr>",
-            self.name, self.ports, self.image, self.id
+            self.name,
+            port_map_list_to_html_ul(&self.ports),
+            self.image,
+            self.id
         )
     }
+}
+
+impl From<&str> for Container {
+    fn from(value: &str) -> Self {
+        // id, ports, name, image
+        let parts = value.trim_matches('"').split(';').collect::<Vec<&str>>();
+        let ports_list = parts[1]
+            .split(',')
+            .map(|p| p.trim())
+            .filter(|p| p.contains('>'))
+            .collect::<Vec<&str>>();
+        println!("{:?}", ports_list);
+        Container {
+            id: parts[0].to_string(),
+            ports: ports_list.into_iter().map(|p| p.into()).collect(),
+            name: parts[2].to_string(),
+            image: parts[3].to_string(),
+        }
+    }
+}
+
+#[derive(Debug, Serialize)]
+pub enum Protocol {
+    Tcp,
+    Udp,
+    Empty,
+    Other,
+}
+
+impl Display for Protocol {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Protocol::Tcp => write!(f, "TCP"),
+            Protocol::Udp => write!(f, "UDP"),
+            Protocol::Empty => write!(f, "Empty"),
+            Protocol::Other => write!(f, "Other"),
+        }
+    }
+}
+
+impl From<&str> for Protocol {
+    fn from(value: &str) -> Self {
+        match value.to_lowercase().as_ref() {
+            "tcp" => Protocol::Tcp,
+            "udp" => Protocol::Udp,
+            "" => Protocol::Empty,
+            _ => Protocol::Other,
+        }
+    }
+}
+
+#[derive(Debug, Serialize)]
+pub struct PortMapping {
+    ip_addr: String,
+    source_port: i32,
+    target_port: i32,
+    protocol: Protocol,
+}
+
+impl PortMapping {
+    fn to_html_list_item(&self) -> String {
+        format!(
+            "<li>{} -> {}, on {} ({})</li>",
+            self.source_port, self.target_port, self.ip_addr, self.protocol
+        )
+    }
+}
+
+impl Display for PortMapping {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.to_html_list_item())
+    }
+}
+
+impl From<&str> for PortMapping {
+    fn from(value: &str) -> Self {
+        let split_protocol = value.split('/').collect::<Vec<&str>>();
+        let protocol: Protocol = split_protocol[1].into();
+        let split_ip_addr = value.split(':').take(1).next().unwrap();
+        let split_ports: Vec<&str> = value.split(&['/', ':', '>'][..]).skip(1).take(2).collect();
+        println!("{:?}", split_ports);
+        let source_port: i32 = split_ports[0]
+            .trim_matches('-')
+            .parse::<i32>()
+            .unwrap_or(-1);
+        let target_port: i32 = split_ports[1]
+            .trim_matches('-')
+            .parse::<i32>()
+            .unwrap_or(-1);
+
+        PortMapping {
+            ip_addr: split_ip_addr.to_string(),
+            source_port,
+            target_port,
+            protocol,
+        }
+    }
+}
+
+pub fn port_map_list_to_html_ul(port_maps: &std::vec::Vec<PortMapping>) -> String {
+    format!(
+        "<ul>{}</ul>",
+        port_maps
+            .iter()
+            .flat_map(|pm| { format!("<li>{}</li>", pm).chars().collect::<Vec<char>>() })
+            .collect::<String>()
+    )
 }
 
 const DOCKER_PS_CMD: &str = "docker";
@@ -34,14 +144,7 @@ pub fn get_container_list() -> std::vec::Vec<Container> {
                     if l.is_empty() {
                         return None;
                     }
-                    // id, ports, name, image
-                    let parts = l.trim_matches('"').split(';').collect::<Vec<&str>>();
-                    Some(Container {
-                        id: parts[0].to_string(),
-                        ports: parts[1].to_string(),
-                        name: parts[2].to_string(),
-                        image: parts[3].to_string(),
-                    })
+                    Some(Container::from(l))
                 })
                 .collect()
         } else {
